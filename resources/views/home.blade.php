1d72f391569f34d1c198d46f4ef96999799938e6
thechrisandrew <67407803+thechrisandrew@users.noreply.github.com>
2022-11-02 16:12:19 +0700

    feat: Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        @guest
+                            <h5 class="card-title">{{ __('You are not logged in!') }}</h5>
+                            <p class="card-text text-muted">{{ __('Please login to post something.') }}</p>
+                        @else
+                            <h5 class="card-title">{{ __('You are logged in as:') }} {{ Auth::user()->username }}</h5>
+                            <p class="card-text text-muted">{{ __("What's on your mind?") }}</p>
+                        @endguest
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
