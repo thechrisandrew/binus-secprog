d60374f309c9d03fa1fe1754b6b8023c5ad19a2e
Chris Andrew <67407803+thechrisandrew@users.noreply.github.com>
2022-11-04 00:49:29 +0700

    feat: Update user model with first and last name (#6)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,41 @@
                                         class="form-control @error('username') is-invalid @enderror" name="username"
                                         value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                    @error('name')
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="firstname"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('First Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="firstname" type="text"
+                                        class="form-control @error('firstname') is-invalid @enderror" name="firstname"
+                                        value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
+
+                                    @error('firstname')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="lastname"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="lastname" type="text"
+                                        class="form-control @error('lastname') is-invalid @enderror" name="lastname"
+                                        value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
+
+                                    @error('lastname')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
