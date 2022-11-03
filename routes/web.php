375bcf1d5e7bc7e350c5766e593d4d1c39da6ff0
Lex <94731863+newdna@users.noreply.github.com>
2022-11-04 00:20:28 +0700

    feat: Update index route (#5)
    * feat: Update index route
    * style: Clean up route web.php file
    
    Co-authored-by: thechrisandrew <67407803+thechrisandrew@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/posts', function(){
     return view('posts');
 });
